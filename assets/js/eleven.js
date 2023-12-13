43c3f0c168552ee9571d89f808d593639e216ac2
srirachy <caseyereyes@gmail.com>
2023-12-12 18:19:32 -0800

    add code to exercise eleven

--- assets/js/eleven.js
+++ assets/js/eleven.js
@@ -1,8 +1,8 @@
 async function getWeather() {
     try{
-        let city = document.getElementById('zipcode').value;
-        let key = 'd72729578543cfc435344f99b226b1c9';
-        let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`
+        const city = document.getElementById('zipcode').value;
+        const key = 'd72729578543cfc435344f99b226b1c9';
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`
         await fetch(url)
         .then((weather) => {return weather.json()})
         .then(displayData);
@@ -14,7 +14,8 @@ async function getWeather() {
 
 function displayData(weather){
     console.log(weather);
-    let div = document.getElementById('test');
-    let fTemp = (Number(weather.main.temp) * 1.8) + 32;
-    div.innerHTML =  fTemp;
+    const div = document.getElementById('test');
+    const fTemp = (Number(weather.main.temp) * 1.8) + 32;
+    const output = `${fTemp}${String.fromCharCode(176)}F`
+    div.innerHTML =  output;
 }
